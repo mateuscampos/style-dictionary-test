9d8483b173504e36456d4f8e0a6701780c345ffb
Mateus de Campos <91mate@gmail.com>
2022-06-09 12:43:35 -0300

    first structure

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Mon, 06 Jun 2022 21:06:16 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
